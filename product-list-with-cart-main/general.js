7657439c10a2191558b4e99b6888953dc98be5c7
Sh1roo0 <kamil.przygrodzki4@gmail.com>
2025-03-17 23:49:55 +0100

    initial_push1

--- product-list-with-cart-main/general.js
+++ product-list-with-cart-main/general.js
@@ -7,33 +7,90 @@ let produkt6 = document.getElementById("produkt6");
 let produkt7 = document.getElementById("produkt7");
 let produkt8 = document.getElementById("produkt8");
 let produkt9 = document.getElementById("produkt9");
+let cartImg = document.getElementById("cartC");
+var p = document.getElementById("ppp");
+
+const cena1 = 6.50;
+const cena2 = 7.00;
+const cena3 = 8.00;
+const cena4 = 5.50;
+const cena5 = 4.00;
+const cena6 = 5.00;
+const cena7 = 4.50;
+const cena8 = 5.50;
+const cena9 = 6.50;
 
 
-let koszyk = document.getElementById("koszyk");
-produkt1.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Waffle with Berries</h3> <br> <p>$6.50</p> <hr>";
-})
-produkt2.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Vanilla Bean Crème Brûlée</h3> <br> <p>$7.00</p> <hr>";
-})
-produkt3.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Macaron Mix of Five</h3>  <br> <p>$8.00</p> <hr>";
-})
-produkt4.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Classic Tiramisu</h3> <br> <p>$5.50</p> <hr>";
-})
-produkt5.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Pistachio Baklava</h3> <br> <p>$4.00</p> <hr>";
-})
-produkt6.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Lemon Meringue Pie</h3> <br> <p>$5.00</p> <hr>" ;
-})
-produkt7.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Red Velvet Cake</h3> <br> <p>$4.50</p> <hr>";
-})
-produkt8.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Salted Caramel Brownie</h3> <br> <p>$4.50</p> <hr>";
-})
-produkt9.addEventListener("click", function(){
-    koszyk.innerHTML += "<h3>Vanilla Panna Cotta</h3> <br> <br> <p>$6.50</p> <hr> ";
-})
+
+
+
+   
+
+   
+    let btntext = document.getElementById("btnTXT");
+    let increment = document.getElementById("increment");
+    let decrement = document.getElementById("decrement");
+    let incSum = document.getElementById("incSum");
+    let count = 1;
+    let count1 = 1;
+    let koszyk = document.getElementById("koszyk");
+    let zamowienie = document.getElementById("zamowienie");
+    const suma = 0;
+    let ycart = document.getElementById("ycart");
+
+    
+        
+
+        produkt1.onclick = function(){
+            if(count >= 0){
+
+            
+                produkt1.style.backgroundColor = "hsl(14, 86%, 42%)";
+                btntext.style.display = "none";
+                incSum.innerHTML = count;
+                
+        
+                increment.style.display = "flex";
+                decrement.style.display = "flex";
+        
+                increment.onclick = function(){
+                    count++;
+                    incSum.textContent = count;
+                }
+                decrement.onclick = function(){
+                    count--;
+                    incSum.textContent = count;
+                }
+
+                cartImg.style.display = "none";
+                p.style.display = "none";
+
+
+                koszyk.innerHTML = `<h3>Waffle with Berries</h3><br> <h5>${count}x</h5>  <h4>@ $${cena1}</h4> <b>$${cena1*count}</b>  <hr>`
+                
+
+                zamowienie.innerHTML = ` Order Total             $${cena1*count} <br>`
+
+                ycart.innerHTML = ` <h1>Your Cart (${count})</h1> `
+                }
+                else if (count < 0){
+                    produkt1.style.backgroundColor = "white";
+                    btntext.style.display = "flex";
+        
+                    increment.style.display = "none";
+                    decrement.style.display = "none";
+
+                    koszyk.innerHTML = ` `;
+                    zamowienie.innerHTML = ` `;
+                    cartImg.style.display = "flex";
+                    p.style.display = "flex";
+                    p.style.justifyContent = "center";
+
+
+        
+                    count++;
+        }
+    }
+
+    
+
